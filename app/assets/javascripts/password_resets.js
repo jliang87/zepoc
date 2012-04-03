138a23eb39350f6f92eec9e1b80409f339a87b0d
jackliang <jackliang1987@hotmail.com>
2012-04-02 22:10:18 -0700

    weird

--- app/assets/javascripts/password_resets.js
+++ app/assets/javascripts/password_resets.js
@@ -2,5 +2,6 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
-$ cool = ->
-  alert "cool"
+$(function cool() {
+    alert("cool")
+})
