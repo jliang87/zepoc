1838497bf1c23e047ee712a275e9399891787ce2
jack liang <jackliang@Macintosh.local>
2012-06-23 19:08:29 -0700

    asd

--- config/initializers/queue_classic.rb
+++ config/initializers/queue_classic.rb
@@ -1,2 +1,2 @@
 ENV["QC_DATABASE_URL"] = "postgres://localhost/zepoc" if Rails.env.development?
-ENV["QC_DATABASE_URL"] = "postgres://postgres:ml870813@zepoc.herokuapp.com/zepoc" if Rails.env.production?
+ENV["DATABASE_URL"] = "postgres://postgres:ml870813@zepoc.herokuapp.com/zepoc" if Rails.env.production?
