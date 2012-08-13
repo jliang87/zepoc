00adce3b1eb76ad9eee6ca93891ef565a8619191
jack liang <jackliang@Macintosh.local>
2012-08-12 21:49:05 -0700

    good modal

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   
-  def gravatar_for(user, options = { size: 300})
+  def gravatar_for(user, options = { size: 100})
     gravatar_id = Digest::MD5::hexdigest(user)
     size = options[:size]
     gravatar_url= "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
