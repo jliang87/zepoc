1edea01cdfbf34d974022f02f3d99d87a12754ca
jack <jackliang1987@hotmail.com>
2012-11-17 22:39:50 -0500

    changedlog

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,8 @@ SampleApp::Application.configure do
 
   # See everything in the log (default is :info)
   config.log_level = :debug
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger::DEBUG
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
