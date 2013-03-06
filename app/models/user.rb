9755d47992167c9b0545913d3b96d344a95508f0
jack <jackliang1987@hotmail.com>
2013-03-06 01:03:18 -0500

    fix removing avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
   before_save { |user| user.email = email.downcase }
   before_save { create_remember_token(:remember_token) }
   #after_update :reprocess_avatar, if: :cropping?     # prevent loop
-  before_save { self.avatar.clear if destroy_avatar == 1 }
+  before_save { self.avatar.clear if destroy_avatar.to_i == 1 }
 
   valid_name_regex=/\A[\w]+\z/i
   validates :name, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 50}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ SampleApp::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  
+
   Paperclip.options[:command_path] = '/usr/local/bin/'
 
   config.action_mailer.default_url_options = {host: "localhost:3000"}
